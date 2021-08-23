00b61a19d106faf99f10b2156dac42864931e345
let-robots-reign <zotov.alexei.1@yandex.ru>
2021-08-23 23:40:39 +0400

    fix GitHub links, add images hover styling via JS

--- js/main.js
+++ js/main.js
@@ -11,3 +11,13 @@ anchors.forEach((anchor) => {
         });
     });
 });
+
+document.querySelectorAll('.section__title-link').forEach((el) => {
+    const toggleHoveredState = (e) => {
+        const correspondingImage = e.target.previousElementSibling;
+        if (correspondingImage !== null) {
+            correspondingImage.classList.toggle('person__image_hovered');
+        }
+    }
+    ['mouseover', 'mouseout'].forEach((event) => el.addEventListener(event, toggleHoveredState));
+});
